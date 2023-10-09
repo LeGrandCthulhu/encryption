808daf041cfdcbb20374601f1dcb74dc23b819bc
LeGrandCthulhu <pierre.frebet@gmail.com>
2023-10-09 19:43:31 +0200

    Still not working

--- encryption.py
+++ encryption.py
@@ -1,8 +1,11 @@
 import encryption_func as func
 
-lettre = ord("B")
+#lettre = ord("B")
 public, private = func.genKeys()
-encrypted = (lettre**public[0]) % public[1]
-print(encrypted, chr(encrypted))
-decrypted = (encrypted**private[0]) % private[1]
-print(decrypted, chr(decrypted))
+print(public, private)
+for lettre in range(private[1] + 20): 
+    encrypted = (lettre**public[0]) % public[1]
+#print(encrypted, chr(encrypted))
+    decrypted = (encrypted**private[0]) % private[1]
+    print(lettre == decrypted)
+#print(decrypted, chr(decrypted))
--- encryption_func.py
+++ encryption_func.py
@@ -3,8 +3,8 @@ from math import gcd
 from sympy import prime
 
 def primeGen() -> tuple[int]:
-    p = prime(randint(1, 10))
-    q = prime(randint(1, 10))
+    p = prime(randint(3, 10))
+    q = prime(randint(3, 10))
     return p, q
 
 def phi(p: int, q: int) -> int:
@@ -32,15 +32,15 @@ def genE(N: int, phiN: int) -> int: # on met phi(N) en paramètre pour éviter d
         if gcd(i, N) == gcd(i, phiN) == 1:
             return i
 
-def genD(e: int, phiN: int) -> int:
-    return randint(4, 15)*phiN + e
+def genD(e: int, phiN: int, N: int) -> int:
+    return (randint(2, (N-e)//phiN)*phiN) + e
 
 def genKeys() -> tuple[tuple[int]]:
     p, q = primeGen()
     N = p * q
     phiN = phi(p, q)
     e = genE(N, phiN)
-    d = genD(e, phiN)
+    d = genD(e, phiN, N)
 
     return ((e, N), (d, N))
 
